3e7cac6e60f28618bd59a40ecbafa136591af07f
Nguyễn Hữu Nam <nam009061999@gmail.com>
2021-07-05 09:41:15 +0700

    more screen

--- app/src/main/java/com/sunasterisk/loltournaments/ui/more/MoreFragment.kt
+++ app/src/main/java/com/sunasterisk/loltournaments/ui/more/MoreFragment.kt
@@ -1,7 +1,45 @@
 package com.sunasterisk.loltournaments.ui.more
 
-import androidx.fragment.app.Fragment
+import android.app.Dialog
+import android.content.Intent
+import androidx.navigation.fragment.findNavController
 import com.sunasterisk.loltournaments.R
+import com.sunasterisk.loltournaments.base.BaseFragment
+import com.sunasterisk.loltournaments.base.BaseViewModel
+import com.sunasterisk.loltournaments.databinding.FragmentMoreBinding
 
-class MoreFragment : Fragment(R.layout.fragment_more){
+class MoreFragment : BaseFragment<FragmentMoreBinding>() {
+    override val layoutResource get() = R.layout.fragment_more
+    override val viewModel: BaseViewModel?
+        get() = null
+
+    override fun initViews() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initActions() {
+        binding.apply {
+            cardAboutApp.setOnClickListener { openAboutAppDialog() }
+            cardShare.setOnClickListener { shareApp() }
+        }
+    }
+
+    private fun openAboutAppDialog() {
+        activity?.let {
+            Dialog(it).apply {
+                setContentView(R.layout.fragment_about_app)
+                show()
+            }
+        }
+    }
+
+    private fun shareApp() {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.link_app))
+        }
+        startActivity(intent)
+    }
 }
